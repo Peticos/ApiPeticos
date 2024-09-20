2866d489d165938a30d3b0a3552a70f47d3bff95
Bianca Machado <bianca.machado@germinare.org.br>
2024-09-20 16:37:11 -0300

    test

--- src/main/java/com/example/apipeticos/controllers/AddressController.java
+++ src/main/java/com/example/apipeticos/controllers/AddressController.java
@@ -23,6 +23,7 @@ public class AddressController {
         this.addressService = addressService;
     }
 
+    //testando
     @GetMapping("/getall")
     public List<Address> getAll(){
         return addressService.findAll();
