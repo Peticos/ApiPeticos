1001be0cc1fcc7ff8feb19e4bd58e5cc17cd5622
Bianca Machado <bianca.machado@germinare.org.br>
2024-09-24 08:16:20 -0300

    DayHint WIP

---
+++ src/main/java/com/example/apipeticos/repositories/DayHintRepository.java
@@ -0,0 +1,9 @@
+package com.example.apipeticos.repositories;
+
+import com.example.apipeticos.models.DayHint;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface DayHintRepository extends JpaRepository<DayHint, Long> {
+
+
+}
