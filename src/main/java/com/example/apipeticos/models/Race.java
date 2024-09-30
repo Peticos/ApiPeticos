86f1c23c5274e0d2a7cbcf4b42e4399ecd6b5501
Bianca Machado <bianca.machado@germinare.org.br>
2024-09-30 09:37:07 -0300

    fixing erros

---
+++ src/main/java/com/example/apipeticos/models/Race.java
@@ -0,0 +1,21 @@
+package com.example.apipeticos.models;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+@Table(name = "Race")
+public class Race {
+    @Id
+    Long id_race;
+    String race;
+}
