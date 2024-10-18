21b8d396f4a29ce14635b6244aaeaa2ab17a06de
Bianca Machado <bianca.machado@germinare.org.br>
2024-10-18 20:44:01 -0300

    adding procedures

--- src/main/java/com/example/apipeticos/api/repositories/UsersRepository.java
+++ src/main/java/com/example/apipeticos/api/repositories/UsersRepository.java
@@ -2,13 +2,20 @@ package com.example.apipeticos.api.repositories;
 
 import com.example.apipeticos.api.models.Users;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.jpa.repository.query.Procedure;
 import org.springframework.data.repository.query.Param;
+
+import java.util.List;
 
 
 public interface UsersRepository extends JpaRepository<Users, Integer> {
 
-    Users findByUsername(String username);
+
+    @Procedure(name = "delete_user")
+    void deleteByUsername(String username_param);
+    @Query(value = "SELECT * FROM select_user(CAST(:username_param AS varchar))", nativeQuery = true)
+    Users findByUsername(@Param("username_param") String username);
 
     @Procedure(procedureName = "insert_user_tutor")
     void insertUserTutor(
@@ -18,7 +25,8 @@ public interface UsersRepository extends JpaRepository<Users, Integer> {
             @Param("bairro") String bairro,
             @Param("plan") String plan,
             @Param("phone") String phone,
-            @Param("gender") String gender
+            @Param("gender") String gender,
+            @Param("profilePicture") String profilPicture
     );
 
     @Procedure(procedureName = "insert_user_profissional")
@@ -29,7 +37,9 @@ public interface UsersRepository extends JpaRepository<Users, Integer> {
             @Param("bairro") String bairro,
             @Param("plan") String plan,
             @Param("phone") String phone,
-            @Param("cnpj") String cnpj
+            @Param("cnpj") String cnpj,
+            @Param("profilePicture") String profilPicture
+
     );
 
     @Procedure(procedureName = "update_user_tutor")
@@ -40,7 +50,9 @@ public interface UsersRepository extends JpaRepository<Users, Integer> {
             @Param("email") String email,
             @Param("bairro") String bairro,
             @Param("phone") String phone,
-            @Param("gender") String gender
+            @Param("gender") String gender,
+            @Param("profilePicture") String profilPicture
+
     );
 
     @Procedure(procedureName = "update_user_profissional")
@@ -51,7 +63,9 @@ public interface UsersRepository extends JpaRepository<Users, Integer> {
             @Param("email") String email,
             @Param("bairro") String bairro,
             @Param("phone") String phone,
-            @Param("cnpj") String cnpj
+            @Param("cnpj") String cnpj,
+            @Param("profilePicture") String profilPicture
+
     );
 
 }
