a7cf6fab2bac6ec514291ba695fe0c24ce4a52c7
Bianca Machado <bianca.machado@germinare.org.br>
2024-10-24 10:52:39 -0300

    Weight WIP

--- src/main/java/com/example/apipeticos/api/models/Weight.java
+++ src/main/java/com/example/apipeticos/api/models/Weight.java
@@ -1,2 +1,24 @@
-package com.example.apipeticos.api.models;public class Weight {
+package com.example.apipeticos.api.models;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.sql.Date;
+
+@Entity
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+public class Weight {
+
+    @Id
+    private Integer idWeight;
+    private Integer idPet;
+    private double weight;
+    private Date weightDate;
 }
--- src/main/java/com/example/apipeticos/api/services/WeightService.java
+++ src/main/java/com/example/apipeticos/api/services/WeightService.java
@@ -1,2 +1,15 @@
-package com.example.apipeticos.api.services;public class WeightService {
+package com.example.apipeticos.api.services;
+
+import com.example.apipeticos.api.repositories.WeightRepository;
+import org.springframework.stereotype.Service;
+
+@Service
+public class WeightService {
+
+    private WeightRepository weightRepository;
+
+    public WeightService (WeightRepository weightRepository){
+        this.weightRepository=weightRepository;
+    }
+
 }
