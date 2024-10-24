a057cfd30400dd3d6f6236e3953195c5db216ef5
Bianca Machado <bianca.machado@germinare.org.br>
2024-10-24 10:41:08 -0300

    first version of vaccine

---
+++ src/main/java/com/example/apipeticos/api/services/VaccineService.java
@@ -0,0 +1,25 @@
+package com.example.apipeticos.api.services;
+
+import com.example.apipeticos.api.models.Vaccine;
+import com.example.apipeticos.api.repositories.VaccineRepository;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class VaccineService {
+
+    private VaccineRepository vaccineRepository;
+
+    public VaccineService(VaccineRepository vaccineRepository){
+        this.vaccineRepository=vaccineRepository;
+    }
+
+    public List<Vaccine> findAll(){
+        return vaccineRepository.findAll();
+    }
+
+    public List<Vaccine> findByIdPet(Integer id){
+        return vaccineRepository.getVaccineByIdPet(id);
+    }
+}
