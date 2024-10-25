7406f10777685da887175251412a91bc89f5f1b7
Bianca Machado <bianca.machado@germinare.org.br>
2024-10-25 16:00:40 -0300

    Swagger Vaccine

--- src/main/java/com/example/apipeticos/api/controllers/VaccineController.java
+++ src/main/java/com/example/apipeticos/api/controllers/VaccineController.java
@@ -3,6 +3,13 @@ package com.example.apipeticos.api.controllers;
 import com.example.apipeticos.api.models.Vaccine;
 import com.example.apipeticos.api.models.Vakinha;
 import com.example.apipeticos.api.services.VaccineService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.ExampleObject;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import org.hibernate.sql.Insert;
 import org.springframework.web.bind.annotation.*;
 
@@ -18,19 +25,48 @@ public class VaccineController {
         this.vaccineService=vaccineService;
     }
 
+    @Operation(summary = "Busca todas as vacinas", description = "Utilizado para testes")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Vacinas do Pet encontradas"),
+            @ApiResponse(responseCode = "500", description = "Erro interno do servidor", content = @Content)
+    })
     @GetMapping("/getall")
     public List<Vaccine> getAll(){
         return vaccineService.findAll();
     }
 
+    @Operation(summary = "Busca vacinas por ID do Pet")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Vacinas do Pet encontradas"),
+            @ApiResponse(responseCode = "500", description = "Erro interno do servidor", content = @Content)
+    })
     @GetMapping("/findbyid/{id}")
-    public List<Vaccine> findByIdPet(@PathVariable Integer id){
+    public List<Vaccine> findByIdPet( @Parameter(description = "ID do Pet para o qual as vacinas serão buscadas", required = true, example = "146") @PathVariable Integer id){
         return vaccineService.findByIdPet(id);
     }
 
-    @PostMapping("/insert")
-    public Integer insert(@RequestBody Vaccine vaccine){
+    @Operation(summary = "Insere uma nova vacina")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "201", description = "Vacina inserida com sucesso. Retorna o Id da Vacina que foi inserida."),
+            @ApiResponse(responseCode = "400", description = "Requisição inválida", content = @Content),
+            @ApiResponse(responseCode = "500", description = "Erro interno do servidor", content = @Content)
+    })
+    @io.swagger.v3.oas.annotations.parameters.RequestBody(
+            content = @Content(mediaType = "application/json",
+                    schema = @Schema(implementation = Vaccine.class),
+                    examples = {
+                            @ExampleObject(name = "Vacina", summary = "Exemplo para inserir vacina",
+                                    value = "{\n" +
+                                            "  \"idPet\": 145, \n" +
+                                            "  \"name\": \"Rabies\",  \n" +
+                                            "  \"numDoses\": 2       \n" +
+                                            "}", description = "Exemplo para inserir vacina. Não é necessário informar o idVacinas, pois a API gera esse número automaticamente. Lembre-se de usar um idPet válido!"),
 
+                    }
+            )
+    )
+    @PostMapping("/insert")
+    public Integer insert( @RequestBody Vaccine vaccine){
         return vaccineService.insertVaccine(vaccine);
     }
 }
--- src/main/java/com/example/apipeticos/api/models/Vaccine.java
+++ src/main/java/com/example/apipeticos/api/models/Vaccine.java
@@ -1,5 +1,6 @@
 package com.example.apipeticos.api.models;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
@@ -16,12 +17,17 @@ import java.sql.Date;
 @NoArgsConstructor
 @Getter
 @Setter
+@Schema(description = "Vacina")
 public class Vaccine {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY) // or GenerationType.AUTO
+    @Schema(description = "Id da vacina",example = "3")
     private Integer idVaccine;
+    @Schema(description = "Id do pet que tem que tomar a vacina", example = "140")
     private Integer idPet;
+    @Schema(description = "Nome da vacina", example = "Raiva")
     private String name;
+    @Schema(description = "Numeros de Dose que a Vacina Tem", example = "2")
     private Integer numDoses;
 }
