d5cf2ab7bdf64634bc1a7028f01d8c95db4006b6
Bianca Machado <bianca.machado@germinare.org.br>
2024-11-06 23:07:43 -0300

    Swagger Weight

--- src/main/java/com/example/apipeticos/api/controllers/WeightController.java
+++ src/main/java/com/example/apipeticos/api/controllers/WeightController.java
@@ -3,6 +3,11 @@ package com.example.apipeticos.api.controllers;
 import com.example.apipeticos.api.models.ApiResponseSQL;
 import com.example.apipeticos.api.models.Weight;
 import com.example.apipeticos.api.services.WeightService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -19,14 +24,20 @@ public class WeightController {
         this.weightService=weightService;
     }
 
+    @Operation(summary = "Insere um novo peso", description = "Insere um registro de peso para um usuário específico.")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Peso inserido com sucesso",
+                    content = @Content(mediaType = "application/json", schema = @Schema(implementation = ApiResponseSQL.class))),
+            @ApiResponse(responseCode = "500", description = "Erro interno do servidor",
+                    content = @Content(mediaType = "application/json", schema = @Schema(implementation = ApiResponseSQL.class)))
+    })
     @PostMapping("/insert")
     public ResponseEntity<ApiResponseSQL> insert(@RequestBody Weight weight){
-
         try{
             weightService.insert(weight);
             return ResponseEntity.ok( new ApiResponseSQL("Peso inserido com sucesso") );
         }catch (Exception e){
-            return ResponseEntity.internalServerError().body(new ApiResponseSQL(""));
+            return ResponseEntity.internalServerError().body(new ApiResponseSQL("Erro ao Inserir"));
         }
     }
 }
