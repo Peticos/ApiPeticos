88e1c3bce1431ee845ead4860bfc3d00587723c1
Bianca Machado <bianca.machado@germinare.org.br>
2024-11-06 23:23:18 -0300

    Swagger Race

--- src/main/java/com/example/apipeticos/api/controllers/RaceController.java
+++ src/main/java/com/example/apipeticos/api/controllers/RaceController.java
@@ -1,10 +1,18 @@
 package com.example.apipeticos.api.controllers;
 
 import com.example.apipeticos.api.models.Race;
+import com.example.apipeticos.api.models.Size;
 import com.example.apipeticos.api.services.RaceService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.HttpServerErrorException;
 
 import java.util.List;
 
@@ -18,8 +26,20 @@ public class RaceController {
         this.raceService = raceService;
     }
 
+    @Operation(summary = "Busca todas as entradas de Race", description = "Retorna uma lista com todas as entradas do tipo Race. Utilizada para cadastro de pet.")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Lista de Race retornada com sucesso",
+                    content = @Content(mediaType = "application/json", schema = @Schema(implementation = Size.class))),
+            @ApiResponse(responseCode = "500", description = "Erro interno do servidor",
+                    content = @Content(mediaType = "application/json"))
+    })
     @GetMapping("/getall")
-    public List<Race> getAll() {
-        return raceService.findAll();
+    public ResponseEntity getAll() {
+        try {
+            return ResponseEntity.ok(raceService.findAll());
+
+        }catch (HttpServerErrorException.InternalServerError e){
+            return ResponseEntity.internalServerError().body("Erro interno no servidor");
+        }
     }
 }
